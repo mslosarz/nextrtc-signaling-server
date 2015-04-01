5fd7c4e513f3940ca0803e476e888aa1a39a44a5
Marcin Ślósarz <m.slosarz@ahej.pl>
2015-04-01 20:42:28 +0200

    fixed test after checkPrecondition modification

--- src/main/java/org/nextrtc/signalingserver/domain/signal/Left.java
+++ src/main/java/org/nextrtc/signalingserver/domain/signal/Left.java
@@ -1,11 +1,14 @@
 package org.nextrtc.signalingserver.domain.signal;
 
 import static org.nextrtc.signalingserver.api.annotation.NextRTCEvents.MEMBER_LEFT;
+import static org.nextrtc.signalingserver.exception.Exceptions.CONVERSATION_NOT_FOUND;
+import static org.nextrtc.signalingserver.exception.Exceptions.INVALID_RECIPIENT;
 
 import org.nextrtc.signalingserver.api.annotation.NextRTCEvents;
 import org.nextrtc.signalingserver.domain.Conversation;
 import org.nextrtc.signalingserver.domain.InternalMessage;
 import org.nextrtc.signalingserver.domain.Member;
+import org.nextrtc.signalingserver.exception.SignalingException;
 import org.nextrtc.signalingserver.repository.Conversations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -41,6 +44,15 @@ public class Left extends AbstractSignal {
 		}
 		conversation.left(message.getFrom());
 
+	}
+
+	protected void checkPrecondition(InternalMessage message, Optional<Conversation> conversation) {
+		if (!conversation.isPresent()) {
+			throw CONVERSATION_NOT_FOUND.exception();
+		}
+		if (!conversation.get().has(message.getFrom())) {
+			throw INVALID_RECIPIENT.exception();
+		}
 	}
 
 	@Override
