027ffc50ac1cd8a834f552a81336d9701150e180
Marcin Ślósarz <m.slosarz@ahej.pl>
2016-09-08 00:04:51 +0200

    stop both streams

--- src/main/resources/nextRTC.js
+++ src/main/resources/nextRTC.js
@@ -160,6 +160,9 @@ function NextRTC(config) {
         if(nextRTC.localStream != null){
             nextRTC.localStream.stop();
         }
+        if(nextRTC.remoteStream != null){
+            nextRTC.remoteStream.stop();
+        }
     };
 
     this.leave = function(){
@@ -168,6 +171,9 @@ function NextRTC(config) {
         nextRTC.signaling.close();
         if(nextRTC.localStream != null){
             nextRTC.localStream.stop();
+        }
+        if(nextRTC.remoteStream != null){
+            nextRTC.remoteStream.stop();
         }
     };
 
