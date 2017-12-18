a40b1cf5c83cc299d633dd34fb035b38ef232206
Marcin Ślósarz <m.slosarz@ahej.pl>
2017-12-18 22:19:21 +0100

    3k sessions

--- src/test/java/org/nextrtc/signalingserver/performance/PerformanceTest.java
+++ src/test/java/org/nextrtc/signalingserver/performance/PerformanceTest.java
@@ -1,29 +1,70 @@
 package org.nextrtc.signalingserver.performance;
 
+import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.client.WebSocketClient;
 import org.junit.Test;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
 
 
 //@Ignore
 public class PerformanceTest {
 
     @Test
-    public void shouldConnectToServer() throws Exception {
+    public void shouldBeAbleToOpen3000Sessions() throws Exception {
         // given
-        WebSocketClient client = new WebSocketClient();
-        MockedClient socket = new MockedClient();
-        URI uri = new URI("ws://localhost:8080/signaling");
-        try {
-            client.start();
-            client.connect(socket, uri).get();
-        } finally {
-            client.stop();
-        }
+        ExecutorService service = Executors.newFixedThreadPool(4);
+
+        List<Future<WebSocketClient>> clients = new ArrayList<>();
+        for (int i = 0; i < 30; i++)
+            clients.add(service.submit(this::open100Sessions));
+
         // when
+        List<WebSocketClient> webClients = clients.stream().map(f -> doTry(f::get)).collect(toList());
 
         // then
+        Thread.sleep(1000000);
+        webClients.forEach(w -> {
+            try {
+                w.stop();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
 
+    private WebSocketClient open100Sessions() {
+        WebSocketClient client = new WebSocketClient();
+        MockedClient socket = new MockedClient();
+        try {
+            URI uri = new URI("ws://localhost:8080/signaling");
+            List<Future<Session>> sessions = new LinkedList<>();
+            client.start();
+            IntStream.range(1, 100).forEach(i -> doTry(() -> sessions.add(client.connect(socket, uri))));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return client;
+    }
+
+    private <T> T doTry(Except<T> supplier) {
+        try {
+            return supplier.execute();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    interface Except<T> {
+        T execute() throws Exception;
     }
 }
